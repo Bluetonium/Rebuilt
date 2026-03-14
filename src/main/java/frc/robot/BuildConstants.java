5948eaef0a80f8e5dcb6ff0cfbdcbcfa10d364e6
Lilliana Wang <lillianawang09@gmail.com>
2026-03-14 16:31:52 -0500

    added target trajectory

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Rebuilt";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 41;
+  public static final String GIT_SHA = "ab2bf69fa3c02a83c6812a8e0675f025d72b8d23";
+  public static final String GIT_DATE = "2026-03-14 12:56:14 EDT";
+  public static final String GIT_BRANCH = "dev";
+  public static final String BUILD_DATE = "2026-03-14 15:19:59 EDT";
+  public static final long BUILD_UNIX_TIME = 1773515999121L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
