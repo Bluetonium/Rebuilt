763d7eca55182bdc952e40f346bf340d137f8cb0
Lilliana Wang <lillianawang09@gmail.com>
2026-03-14 15:30:46 -0500

    added autoAim class & advantageScope

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,18 +4,25 @@
 
 package frc.robot;
 
-import java.util.logging.Logger;
+import org.littletonrobotics.junction.Logger;
+
+import java.util.List;
 
 import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 
 import com.ctre.phoenix6.HootAutoReplay;
 import com.ctre.phoenix6.SignalLogger;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.util.sendable.SendableRegistry;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -26,7 +33,6 @@ public class Robot extends LoggedRobot {
     private Command m_autonomousCommand;
 
     private final RobotContainer m_robotContainer;
-    private final Field2d m_field = new Field2d();
 
     @SuppressWarnings("unused")
     private RobotSim sim;// whoopsy you forgot to make this so the sim doesnt work, i fixed it though C:
@@ -44,21 +50,48 @@ public class Robot extends LoggedRobot {
         SendableRegistry.add(CommandScheduler.getInstance(),"Command Scheduler");
         SmartDashboard.putData(CommandScheduler.getInstance());
 
-        // Logger.recordMetadata("ProjectName", "MyProject");
-        // Logger.start();
+        Logger.recordMetadata("Rebuilt", "MyProject");
+        if (RobotBase.isReal()) {
+            Logger.addDataReceiver(new WPILOGWriter()); // write logs to USB
+        }
+        Logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
+
+        Logger.addDataReceiver(new NT4Publisher());
+        Logger.start();
     }
 
     @Override
     public void robotInit() {
-        DataLogManager.start();
-        DriverStation.startDataLog(DataLogManager.getLog());
-        // Logger.recordOutput("RobotPose", m_robotContainer.drivetrain.getPose());
     }
 
     @Override
     public void robotPeriodic() {
         m_timeAndJoystickReplay.update();
         CommandScheduler.getInstance().run();
+
+        Pose2d robotPose = m_robotContainer.drivetrain.getState().Pose;
+
+        double length = 6.0;
+
+        Translation2d frontTranslation =
+            robotPose.getTranslation().plus(
+                new Translation2d(length, 0).rotateBy(robotPose.getRotation())
+            );
+
+        Pose2d frontPose = new Pose2d(frontTranslation, robotPose.getRotation());
+
+        Trajectory headingTrajectory = new Trajectory(
+            List.of(
+                new Trajectory.State(0, 0, 0, robotPose, 0),
+                new Trajectory.State(0, 0, 0, frontPose, 0)
+            )
+        );
+
+        Pose2d targetPoint = new Pose2d(RobotContainer.getHubX(), 4.034, new Rotation2d());
+
+        Logger.recordOutput("Field/TargetPoint", targetPoint);
+        Logger.recordOutput("Robot/Pose", robotPose);
+        Logger.recordOutput("Robot/Heading", headingTrajectory);
     }
 
     @Override
@@ -102,37 +135,10 @@ public class Robot extends LoggedRobot {
 
             CommandScheduler.getInstance().cancel(m_autonomousCommand);
         }
-        SmartDashboard.putData("Field", m_field);
     }
 
     @Override
     public void teleopPeriodic() {
-        Pose2d pose = m_robotContainer.drivetrain.getState().Pose;
-
-        // update robot position
-        m_field.setRobotPose(pose);
-
-        Translation2d end =
-            pose.getTranslation().plus(
-                new Translation2d(2, pose.getRotation())
-            );
-
-        Translation2d end2 =
-            pose.getTranslation().plus(
-                new Translation2d(4, pose.getRotation())
-            );
-
-        Translation2d end3 =
-            pose.getTranslation().plus(
-                new Translation2d(6, pose.getRotation())
-            );
-
-        m_field.getObject("heading").setPoses(
-            pose,
-            new Pose2d(end, pose.getRotation()),
-            new Pose2d(end2, pose.getRotation()),
-            new Pose2d(end3, pose.getRotation())
-        );
     }
 
     @Override
