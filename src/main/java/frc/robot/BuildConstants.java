d99b2153d04f6487835eb100a04764cb3afc74ec
Lilliana Wang <lillianawang09@gmail.com>
2026-03-15 01:47:06 -0500

    improved and added autons

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 56;
-  public static final String GIT_SHA = "3ddab4914398d7e8d6bd215f0f1a64bdbef82c8f";
-  public static final String GIT_DATE = "2026-03-14 20:53:58 EDT";
-  public static final String GIT_BRANCH = "Shooter/Simulation";
-  public static final String BUILD_DATE = "2026-03-14 21:11:29 EDT";
-  public static final long BUILD_UNIX_TIME = 1773537089420L;
+  public static final int GIT_REVISION = 60;
+  public static final String GIT_SHA = "b608c72e791dbc9cec45873537bfc2568e2f2bcb";
+  public static final String GIT_DATE = "2026-03-14 22:13:15 EDT";
+  public static final String GIT_BRANCH = "auton";
+  public static final String BUILD_DATE = "2026-03-15 02:43:03 EDT";
+  public static final long BUILD_UNIX_TIME = 1773556983285L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
